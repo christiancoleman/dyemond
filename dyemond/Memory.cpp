604a133e424e1ac4d2c4d1ed99ad52696881b4bf
Christian Coleman <christianlanecoleman@gmail.com>
2018-09-17 11:33:08 -0400

    initial commit

---
+++ dyemond/Memory.cpp
@@ -0,0 +1,158 @@
+#include "pch.h"
+#include "Memory.h"
+
+/*
+	Used:
+		* Game Hacking - Developing Autonomous Bots for Online Games
+*/
+
+/////////////////////////////////////////////////////////////////////////
+// public ///////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+/*
+	Page 120 of Game Hacking book
+	'Opening the Game's Process Identifier'
+*/
+DWORD Memory::getGamePID() {
+	HWND myWindow = FindWindow(NULL, L"FINAL FANTASY XIV"); // TODO: use public variable
+	DWORD PID;
+	GetWindowThreadProcessId(myWindow, &PID);
+	return PID;
+}
+
+/*
+	Page 121-122 of Game Hacking book
+	'Working with OpenProcess()'
+*/
+HANDLE Memory::openProcess() {
+	DWORD PID = this->getGamePID();
+	HANDLE process = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, FALSE, PID);
+	if (process == INVALID_HANDLE_VALUE) {
+		printf("Failed to open PID %d, error code %d", PID, GetLastError());
+		return NULL;
+	}
+	return process;
+}
+
+/*
+	Page 129-130 of Game Hacking book
+	'Bypassing ASLR in Production'
+*/
+DWORD Memory::getBaseAddress(HANDLE process) {
+	DWORD newBase;
+
+	// get the address of kernel32.dll
+	HMODULE kernel32 = GetModuleHandle(L"kernel32.dll");
+
+	// get the address of GetModuleHandle()
+	LPVOID getModuleHandlePtr = GetProcAddress(kernel32, "GetModuleHandleA");
+	if (!getModuleHandlePtr) {
+		getModuleHandlePtr = GetProcAddress(kernel32, "GetModuleHandleW");
+	}
+
+	// create thread
+	HANDLE thread = CreateRemoteThread(
+		process,
+		NULL,
+		NULL,
+		(LPTHREAD_START_ROUTINE)getModuleHandlePtr,
+		NULL,
+		NULL,
+		NULL
+	);
+
+	// let the thread finish
+	WaitForSingleObject(thread, INFINITE);
+
+	// get the exit code
+	GetExitCodeThread(thread, &newBase);
+
+	// clean up the thread handle
+	CloseHandle(thread);
+
+	return newBase;
+}
+
+/*
+	Page 123-124 of Game Hacking book
+	'Writing Templated Memory Access Functions'
+*/
+template<typename T>
+T Memory::readMemory(HANDLE process, LPVOID address) {
+	T val;
+	ReadProcessMemory(process, address, &val, sizeof(T), NULL);
+	return val;
+}
+
+/*
+	Page 123-124 of Game Hacking book
+	'Writing Templated Memory Access Functions'
+*/
+template<typename T>
+void Memory::writeMemory(HANDLE process, LPVOID address, T value) {
+	WriteProcessMemory(process, address, &value, sizeof(T), NULL);
+}
+
+/*
+	Page 127 of Game Hacking book
+	'Changing Memory Protection'
+*/
+template<typename T>
+DWORD Memory::protectMemory(HANDLE process, LPVOID address, DWORD newProtection) {
+	DWORD oldProtection;
+	VirtualProtectEx(process, address, sizeof(T), newProtection, &oldProtection);
+	return oldProtection;
+}
+
+/*
+	Page 128-129 of Game Hacking book
+	'Bypassing ASLR in Production'
+*/
+DWORD Memory::rebase(DWORD address, DWORD newBase) {
+	DWORD diff = address - 0x400000;
+	return diff + newBase;
+}
+
+/////////////////////////////////////////////////////////////////////////
+// private //////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+/*
+template<typename T>
+T readMemory(DWORD address)
+{
+	return *((T*)address);
+}
+
+template<typename T>
+T* pointMemory(DWORD address)
+{
+	return ((T*)address);
+}
+
+template<typename T>
+void writeMemory(DWORD address, T value)
+{
+	*((T*)address) = value;
+}
+
+template<typename T>
+DWORD protectMemory(DWORD address, DWORD protection)
+{
+	DWORD oldProt;
+	VirtualProtect((LPVOID)address, sizeof(T), protection, &oldProt);
+	return oldProt;
+}
+
+struct _creature
+{
+	_creature(int hb, bool e, bool c) : healthBar(hb), isEnemy(e), isCloaked(c) {}
+	int healthBar;
+	bool isEnemy, isCloaked;
+};
+void NOPExample();
+void callHookExample();
+void VFHookExample();
+void IATHookExample();
+*/
---
+++ dyemond/Memory.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Memory {
+
+	public:
+
+		/*
+			Manually set to window title of game for now
+		*/
+		wstring GAME_NAME = L"FINAL FANTASY XIV";
+
+		/*
+
+		*/
+		DWORD getGamePID();
+
+		/*
+
+		*/
+		HANDLE openProcess();
+
+		/*
+			
+		*/
+		DWORD getBaseAddress(HANDLE);
+
+		/*
+		
+		*/
+		template<typename T>
+		T readMemory(HANDLE, LPVOID);
+
+		/*
+			
+		*/
+		template<typename T>
+		void writeMemory(HANDLE, LPVOID, T);
+
+		/*
+			
+		*/
+		template<typename T>
+		DWORD protectMemory(HANDLE, LPVOID, DWORD);
+
+		/*
+			
+		*/
+		DWORD rebase(DWORD, DWORD);
+
+	private:
+
+		
+
+};
